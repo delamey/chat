8bf96077aaf006485c27c051174c2cb8c0ee4363
delamey <dalianmiwei@126.com>
2017-03-08 16:07:46 +0800

    2017030802

--- app/src/main/java/com/example/dell/chat/LoginActivity.java
+++ app/src/main/java/com/example/dell/chat/LoginActivity.java
@@ -1,12 +1,51 @@
 package com.example.dell.chat;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import static com.example.dell.chat.MyApplication.getContext;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 private Toolbar toolbar;
+    private EditText password,account;
+    private Button register,login;
+    String passwordString;
+    String accountString;
+    Handler handler=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case 1:
+                    String R=msg.obj.toString();
+                    if (R=="true"){
+                  Toast.makeText(getApplicationContext(),"注册成功，请登录",Toast.LENGTH_SHORT).show();
+              }else {
+                  Toast.makeText(getApplicationContext(),"注册失败，账号或者密码已经存在",Toast.LENGTH_SHORT).show();
+              }
+              break;
+                case 2:
+                    String R1=msg.obj.toString();
+                    if (R1=="true"){
+                  Toast.makeText(getContext(),"注册成功，请登录",Toast.LENGTH_SHORT).show();
+              }else {
+                  Toast.makeText(getContext(),"注册失败，账号或者密码已经存在",Toast.LENGTH_SHORT).show();
+              }
+              break;
+                default:
+                    break;
+
+            }
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +53,60 @@ private Toolbar toolbar;
         toolbar= (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         toolbar.setTitle("欢迎使用Chat");
+        password= (EditText) findViewById(R.id.PasswordEdit);
+        account= (EditText) findViewById(R.id.AccountEdit);
+        register= (Button) findViewById(R.id.ZhuCe);
+        login= (Button) findViewById(R.id.loginButton);
+        register.setOnClickListener(this);
+        login.setOnClickListener(this);
+        //Http.sendPost("http://localhost:8080/chat/Register","name="+accountString+"&"+"password="+passwordString);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.ZhuCe:
+                new  Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        passwordString=password.getText().toString();
+                        accountString=account.getText().toString();
+                        String receive= Http.sendPost("http://localhost:8080/chat2/Register","name="+accountString+"&"+"password="+passwordString);
+                        Message message=new Message();
+                        message.obj=receive;
+                        message.what=1;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+               //String receive= Http.sendPost("http://localhost:8080/chat/Register","name="+accountString+"&"+"password="+passwordString);
+//              if (receive=="true"){
+//                  Toast.makeText(this,"注册成功，请登录",Toast.LENGTH_SHORT).show();
+//              }else {
+//                  Toast.makeText(this,"注册失败，账号或者密码已经存在",Toast.LENGTH_SHORT).show();
+//              }
+              break;
+            case R.id.loginButton:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String receive1= Http.sendPost("http://localhost:8080/chat/login","name="+accountString+"&"+"password="+passwordString);
+                        Message message=new Message();
+                        message.obj=receive1;
+                        message.what=2;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+               break;
+            default:
+                break;
+//                 if (receive1=="true"){
+//                     Intent intent= new Intent(LoginActivity.this,MainActivity.class);
+//                     startActivity(intent);
+//                 }else {
+//                     Toast.makeText(this,"账号密码错误请重新输入",Toast.LENGTH_SHORT).show();
+//                     password.setText("");
+//                     account.setText("");
+//                 }
+        }
     }
 }
